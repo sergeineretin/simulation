72a10f36fe21e21c3cb9a49ed1cdb65dad847f54
Sergei Neretin <sergeizzzneretin@gmail.com>
2023-09-15 15:08:27 +0700

    initial commit

---
+++ src/main/java/com/sergeineretin/simulation/Main.java
@@ -0,0 +1,7 @@
+package com.sergeineretin.simulation;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/sergeineretin/simulation/MainTests.java
@@ -0,0 +1,4 @@
+package com.sergeineretin.simulation;
+
+public class MainTests {
+}
